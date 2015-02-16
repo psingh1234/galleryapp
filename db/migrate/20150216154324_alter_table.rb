49d6954da584fce90185bf9d327cb834f48b9de4
Ravi Parkash <rprakash@enbake.com>
2015-02-16 21:30:25 +0530

    uploadfile

--- db/migrate/20150216154324_alter_table.rb
+++
@@ -1,6 +0,0 @@
-class AlterTable < ActiveRecord::Migration
-  def change
-    
-    add_column :tag, :upload_id, :integer
-  end
-end
--- db/migrate/20150216155211_tbmigration.rb
+++ db/migrate/20150216155211_tbmigration.rb
@@ -2,7 +2,7 @@ class Tbmigration < ActiveRecord::Migration
   def change
     create_table :tag do |t|
       t.string :name
-
+t.upload_id :integer
       
     end
   end
